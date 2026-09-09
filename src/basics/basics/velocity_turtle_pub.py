373ea7afdc34f987fd74481e6ff66013d0432f81
elenaslunap <elenaaslunaap@gmail.com>
2026-09-09 14:03:25 -0600

    Agregar comentarios al código

--- src/basics/basics/velocity_turtle_pub.py
+++ src/basics/basics/velocity_turtle_pub.py
@@ -7,15 +7,16 @@ class VelocityTurtlePublisher(Node):
 
     def __init__(self):
         super().__init__('twist_publisher')
-
+        # Publisher que manda mensajes de tipo Twist al tópico '/turtle1/cmd_vel con una cola de tamaño 10
         self.publisher_ = self.create_publisher(
             Twist,
             '/turtle1/cmd_vel',
             10
         )
-
+        # Velocidad comienza en 0
         self.Vel = 0.0
 
+        # Se llama a publish_twist cada 0.5 segundos
         self.timer_ = self.create_timer(
             0.5,
             self.publish_twist
@@ -25,13 +26,14 @@ class VelocityTurtlePublisher(Node):
     def publish_twist(self):
 
         msg = Twist()
-
+        
         msg.linear.x = self.Vel
         
-        if self.Vel < 1.2:
+        # Si la velocidad es menor o igual a 1.2, aumenta por 0.1
+        if self.Vel <= 1.2:
             self.publisher_.publish(msg)
             self.Vel = round(self.Vel + 0.1, 1)
-    
+        # Si la velocidad es mayor a 1.2, se publica una velocidad de 0.0 para detener a la tortuga 
         else:
             msg.linear.x = 0.0
             self.publisher_.publish(msg)
--- src/basics/basics/velocity_turtle_subscriber.py
+++ src/basics/basics/velocity_turtle_subscriber.py
@@ -5,10 +5,12 @@ from geometry_msgs.msg import Twist
 
 class VelocityTurtleSubscriber(Node):
 
+    # Se crea la subscripción al tópico turtle1/cmd_vel
     def __init__(self):
         super().__init__('twist_subscriber')
         self.subscription_ = self.create_subscription(Twist,'/turtle1/cmd_vel',self.twist_callback,10)
 
+    # Se imprime en la consola la velocidad recibida
     def twist_callback(self, msg):
         linear_x = msg.linear.x
         self.get_logger().info(f'msg.linear.x = {linear_x:.1f} m/s')
